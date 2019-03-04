cf473729f43a3b5229ec3f735a213fb72c4cdedc
YjjTT <yjjtt@tempot.com>
2019-03-04 17:53:06 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(p1).on('click', function () {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+$(p2).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-300px)'
+    })
+})
+$(p3).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-600px)'
+    })
+})
