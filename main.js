9210e103c1842dc41a376338ef65a31fbac7310e
YjjTT <yjjtt@tempot.com>
2019-03-05 17:24:27 +0800

    优化js代码，封装

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-// $(p1).on('click', function () {
-//     $(images).css({
-//         transform: 'translateX(0)'
-//     })
-// })
-// $(p2).on('click', function () {
-//     $(images).css({
-//         transform: 'translateX(-300px)'
-//     })
-// })
-// $(p3).on('click', function () {
-//     $(images).css({
-//         transform: 'translateX(-600px)'
-//     })
-// })
-var allButtons = $('#buttons > button')
-console.log(allButtons)
-for(let i=0;i<allButtons.length;i++){
-    $(allButtons[i]).on('click', function(x){
-        var index = $(x.currentTarget).index()
-        var px = index * -300
-        $(images).css({
-            transform: 'translateX(' + px + 'px)'
-        })
-    })
+var allButtons = $("#buttons > button");
+console.log(allButtons);
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on("click", function(x) {
+    var index = $(x.currentTarget).index();
+    var px = index * -300;
+    $(images).css({
+      transform: "translateX(" + px + "px)"
+    });
+    n = index;
+    activeButton(allButtons.eq(n % size));
+  });
+}
+
+var n = 0;
+var size = allButtons.length;
+playSlide(n % size)
+
+var timerId = setTimer()
+
+$(".window")
+  .on("mouseenter", function() {
+    window.clearInterval(timerId);
+  })
+  .on("mouseleave", function() {
+    timerId = setTimer()
+  });
+  
+function setTimer() {
+    return setInterval(() => {
+        n += 1;
+        playSlide(n % size)
+      }, 1000);
+}
+function activeButton($button) {
+  $button.addClass("red").siblings(".red").removeClass("red");
+}
+function playSlide(index){
+    allButtons.eq(n % size).trigger("click");
 }
