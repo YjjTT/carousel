849367be6608b30e74af054c082a3e2b6113845b
YjjTT <yjjtt@tempot.com>
2019-03-05 16:47:32 +0800

    优化js， 支持无限张图片

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-$(p1).on('click', function () {
-    $(images).css({
-        transform: 'translateX(0)'
+// $(p1).on('click', function () {
+//     $(images).css({
+//         transform: 'translateX(0)'
+//     })
+// })
+// $(p2).on('click', function () {
+//     $(images).css({
+//         transform: 'translateX(-300px)'
+//     })
+// })
+// $(p3).on('click', function () {
+//     $(images).css({
+//         transform: 'translateX(-600px)'
+//     })
+// })
+var allButtons = $('#buttons > button')
+console.log(allButtons)
+for(let i=0;i<allButtons.length;i++){
+    $(allButtons[i]).on('click', function(x){
+        var index = $(x.currentTarget).index()
+        var px = index * -300
+        $(images).css({
+            transform: 'translateX(' + px + 'px)'
+        })
     })
-})
-$(p2).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-300px)'
-    })
-})
-$(p3).on('click', function () {
-    $(images).css({
-        transform: 'translateX(-600px)'
-    })
-})
+}
